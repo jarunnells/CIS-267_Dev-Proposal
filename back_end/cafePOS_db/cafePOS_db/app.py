5b6a2aa60132d12e6cccdd506dfb621c08217f79
J.A. Runnells [MacBookPro] <48547460+jarunnells@users.noreply.github.com>
2020-11-14 19:13:27 -0700

    moved old into OLD - restructured dir tree

---
+++ back_end/cafePOS_db/cafePOS_db/app.py
@@ -0,0 +1,7 @@
+# ==========================================
+#     DEVELOPER:  J.A. Runnells
+#          FILE:  app.py
+#        BRANCH: dev
+# ==========================================
+
+print("Hellow, World!")
---
+++ back_end/cafePOS_db/setup.py
@@ -0,0 +1,26 @@
+# ==========================================
+#     DEVELOPER:  J.A. Runnells
+#          FILE:  setup.py
+#        BRANCH: dev
+# ==========================================
+from setuptools import setup
+
+with open("README", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name=None,
+    version=None,
+    description=None,
+    license="MIT",
+    long_description=long_description,
+    author="J.A. Runnells",
+    author_email="jason@mztechsolutions.com",
+    url="https://mzTechSolutions.com/",
+    packages=[None],  # same as name
+    install_requires=[None, None],  # external packages as dependencies
+    scripts=[
+        None,
+        None,
+    ]
+)
