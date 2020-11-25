c5caf6681e84aa6e3b7d894cff8ebaf326a33a47
J.A. Runnells [MacBookPro] <48547460+jarunnells@users.noreply.github.com>
2020-11-25 02:31:47 -0700

    refactor -> cosmetic

--- back_end/cafePOS_db_gui/cafePOS_db_gui/modules/config.py
+++ back_end/cafePOS_db_gui/cafePOS_db_gui/modules/config.py
@@ -20,9 +20,24 @@ class Directories:
     # TODO: rename -> DUMP_FILE_NAME ???
     FILE_NAME = f"dump_cafePOS_db-{TIMESTAMP}"
     PROJ_ROOT = "/cafePOS_db/cafePOS_db"
-    DUMP_SQL = {"prefix": "/backup/", "file_name": FILE_NAME, "ext": ".sql", "mode": "w"}
-    DUMP_JSON = {"prefix": "/data/", "file_name": FILE_NAME, "ext": ".json", "mode": "w"}
-    DUMP_CSV = {"prefix": "/data/", "file_name": FILE_NAME, "ext": ".csv", "mode": "w"}
+    DUMP_SQL = {
+        "prefix": "/backup/", 
+        "file_name": FILE_NAME, 
+        "ext": ".sql", 
+        "mode": "w"
+    }
+    DUMP_JSON = {
+        "prefix": "/data/", 
+        "file_name": FILE_NAME, 
+        "ext": ".json", 
+        "mode": "w"
+    }
+    DUMP_CSV = {
+        "prefix": "/data/", 
+        "file_name": FILE_NAME, 
+        "ext": ".csv", 
+        "mode": "w"
+    }
 
 class TableNames:
     CAFE_ITEMS = "items"
@@ -30,16 +45,32 @@ class TableNames:
 
 # TODO: integrate Messages into per Query as dictionary 
 class Messages:
-    UPDATE = {"title": "CONFIRM UPDATE OPERATION!", "message": f"Please confirm update operation on item:"}
-    DELETE = {"title": "CONFIRM DELETE OPERATION!", "message": f"Please confirm delete operation on item:"}
+    UPDATE = {
+        "title": "CONFIRM UPDATE OPERATION!", 
+        "message": f"Please confirm update operation on item:"
+        }
+    DELETE = {
+        "title": "CONFIRM DELETE OPERATION!", 
+        "message": f"Please confirm delete operation on item:"
+        }
     ERRORS = {
-        "add_item": {"title": "ADD Item Error", "message": "ALL fields required!"},
-        "update_item": {"title": "UPDATE Item Error", "message": "ALL fields required!"},
-        "remove_item": {"title": "REMOVE Item Error", "message": "Click an item row first!"}
+        "add_item": {
+            "title": "ADD Item Error", 
+            "message": "ALL fields required!"
+        },
+        "update_item": {
+            "title": "UPDATE Item Error", 
+            "message": "ALL fields required!"
+        },
+        "remove_item": {
+            "title": "REMOVE Item Error", 
+            "message": "Click an item row first!"
+        }
     }
 
 class Query:
-    INIT = f"BEGIN TRANSACTION; CREATE TABLE IF NOT EXISTS {TableNames.ACTIVE_TABLE} (id TEXT PRIMARY KEY, category TEXT NOT NULL, name TEXT NOT NULL, label TEXT NOT NULL, price REAL NOT NULL); COMMIT;"
+    INIT = f"BEGIN TRANSACTION; CREATE TABLE IF NOT EXISTS {TableNames.ACTIVE_TABLE} \
+        (id TEXT PRIMARY KEY, category TEXT NOT NULL, name TEXT NOT NULL, label TEXT NOT NULL, price REAL NOT NULL); COMMIT;"
     FETCH_ALL_TABLES = "SELECT name FROM sqlite_master WHERE type='table'"
     FETCH_ALL_RECORDS = f"SELECT * FROM {TableNames.ACTIVE_TABLE}"
     INSERT = f"INSERT INTO {TableNames.ACTIVE_TABLE} VALUES (?, ?, ?, ?, ?)"
@@ -49,16 +80,56 @@ class Query:
     DROP = f"DROP TABLE IF EXISTS {TableNames.ACTIVE_TABLE}"
 
 class Colors:
-    LIGHT_STEEL_BLUE = {"py_name": "light steel blue", "hex": "#B0C4DE", "rgb": (176, 196, 222)}
-    LIGHT_CORAL = {"py_name": "light coral", "hex": "#F08080", "rgb": (240, 128, 128)}
-    LAVENDER = {"py_name": "lavender", "hex": "#E6E6FA", "rgb": (230, 230, 250)}
-    ANTIQUE_WHITE = {"py_name": "antique white", "hex": "#FAEBD7", "rgb": (250, 235, 215)}
-    DARK_SEA_GREEN = {"py_name": None, "hex": "#8FBC8F", "rgb": (143, 188, 143)}
-    STEEL_BLUE = {"py_name": None, "hex": "#4682B4", "rgb": (70, 130, 180)}
-    FIRE_BRICK = {"py_name": None, "hex": "#B22222", "rgb": (178, 34, 34)}
-    FIRE_BRICK_65 = {"py_name": None, "hex": "#E26969", "rgb": (226, 105, 105)}
-    WHITE_SMOKE = {"py_name": None, "hex": "#F5F5F5", "rgb": (245, 245, 245)}
-    GAINSBORO = {"py_name": None, "hex": "#DCDCDC", "rgb": (220, 220, 220)}
+    LIGHT_STEEL_BLUE = {
+        "py_name": "light steel blue", 
+        "hex": "#B0C4DE", 
+        "rgb": (176, 196, 222)
+    }
+    LIGHT_CORAL = {
+        "py_name": "light coral", 
+        "hex": "#F08080", 
+        "rgb": (240, 128, 128)
+    }
+    LAVENDER = {
+        "py_name": "lavender", 
+        "hex": "#E6E6FA", 
+        "rgb": (230, 230, 250)
+    }
+    ANTIQUE_WHITE = {
+        "py_name": "antique white", 
+        "hex": "#FAEBD7", 
+        "rgb": (250, 235, 215)
+    }
+    DARK_SEA_GREEN = {
+        "py_name": None, 
+        "hex": "#8FBC8F", 
+        "rgb": (143, 188, 143)
+    }
+    STEEL_BLUE = {
+        "py_name": None, 
+        "hex": "#4682B4", 
+        "rgb": (70, 130, 180)
+    }
+    FIRE_BRICK = {
+        "py_name": None, 
+        "hex": "#B22222", 
+        "rgb": (178, 34, 34)
+    }
+    FIRE_BRICK_65 = {
+        "py_name": None, 
+        "hex": "#E26969", 
+        "rgb": (226, 105, 105)
+    }
+    WHITE_SMOKE = {
+        "py_name": None, 
+        "hex": "#F5F5F5", 
+        "rgb": (245, 245, 245)
+    }
+    GAINSBORO = {
+        "py_name": None, 
+        "hex": "#DCDCDC", 
+        "rgb": (220, 220, 220)
+    }
 
 class StylesTtk:
     THEMES = ("default", "classic", "clam", "aqua", "alt")
@@ -101,15 +172,52 @@ class ButtonWidget:
     pady = 5
     width = 12
     BTN = {
-        "01": {"text": "Add Item", "row": 0, "column": 2},
-        "02": {"text": "Remove Item", "row": 0, "column": 3},
-        "03": {"text": "Update Item", "row": 1, "column": 2},
-        "04": {"text": "Clear Input", "row": 1, "column": 3},
-        "05": {"text": "Import DB", "row": 2, "column": 2},                # IMPORT -> CSV, JSON, SQL
-        "06": {"text": "Export DB", "row": 2, "column": 3},                # EXPORT -> CSV, JSON, SQL
-        "07": {"text": "Admin", "row": 3, "column": 2, "columnspan": 2},   # ADMIN -> CREATE_TBL, DROP_TBL, DUMP:=backup.sql, DUMP:=items.json
-        "08": {"text": "Search", "row": 4, "column": 2},
-        "09": {"text": "Reload", "row": 4, "column": 3}
+        "01": {
+            "text": "Add Item", 
+            "row": 0, 
+            "column": 2
+        },
+        "02": {
+            "text": "Remove Item", 
+            "row": 0, 
+            "column": 3
+        },
+        "03": {
+            "text": "Update Item", 
+            "row": 1, 
+            "column": 2
+        },
+        "04": {
+            "text": "Clear Input", 
+            "row": 1, 
+            "column": 3
+        },
+        "05": { # IMPORT -> CSV, JSON, SQL
+            "text": "Import DB", 
+            "row": 2, 
+            "column": 2
+        },
+        "06": { # EXPORT -> CSV, JSON, SQL
+            "text": "Export DB", 
+            "row": 2, 
+            "column": 3
+        },
+        "07": { # ADMIN -> CREATE_TBL, DROP_TBL, DUMP:=backup.sql, DUMP:=items.json
+            "text": "Admin", 
+            "row": 3, 
+            "column": 2, 
+            "columnspan": 2
+        },
+        "08": {
+            "text": "Search", 
+            "row": 4, 
+            "column": 2
+        },
+        "09": {
+            "text": "Reload", 
+            "row": 4, 
+            "column": 3
+        }
     }
     cnf = {
         "width": width,
@@ -157,7 +265,11 @@ class TreeViewWidget:
         "str": ["Item ID", "Category", "Name", "Label", "Price"],
         "int": (1,2,3,4,5)
     }
-    selectmode = {"one": "browse", "many": "extended", "none": "none"}
+    selectmode = {
+        "one": "browse", 
+        "many": "extended", 
+        "none": "none"
+    }
     show = "headings"
     HEADINGS = ["Item ID", "Category", "Name", "Label", "Price"]
     # ===== UNUSED ======
@@ -211,7 +323,11 @@ class BarScroll:
     column = 3
     sticky = tk.W
     cnf = {}
-    cnf_grid = {"row": row, "column": column, "sticky": sticky}
+    cnf_grid = {
+        "row": row, 
+        "column": column, 
+        "sticky": sticky
+    }
     # ===== UNUSED ======
     # command = None
     # name = None
